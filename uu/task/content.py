75440f1fec91676f29bc0e70c56a356820776b23
Alex Clark <aclark@aclark.net>
2015-06-18 19:01:39 -0400

    s/plone.directives.form/plone.supermodel.model/
    Replace grokkable form model with "pure" supermodel model (p.d.form uses p.supermodel model by way of plone.autoform)

--- uu/task/content.py
+++ uu/task/content.py
@@ -1,15 +1,15 @@
-from plone.directives import form
+from plone.supermodel import model
 from zope import schema
 
 
-class IUUTask(form.Schema):
+class IUUTask(model.Schema):
     """
     Assignable Task With Due Date; Contains Dependent Tasks
     """
     title = schema.TextLine(title=u"Name")
 
 
-class IUUTaskPlanner(form.Schema):
+class IUUTaskPlanner(model.Schema):
     """
     Task Container; Shared Configuration For Contained Tasks
     """
