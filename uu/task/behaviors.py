9042e3c0cd2a24157c144b59a1801d06b90608b6
Alex Clark <aclark@aclark.net>
2015-06-19 19:15:53 -0400

    Add files; clarify file layout
    On second thought re: comments in #10 about file layout, I think I prefer:
    
    - content.py for content type schemas not provided by behaviors (if any)
    - behaviors.py for behavior related interfaces & schema
    - interfaces.py for marker interfaces

---
+++ uu/task/behaviors.py
@@ -0,0 +1,5 @@
+from plone.supermodel import model
+
+
+class IUUAssignedTask(model.Schema):
+    """Adds due_on datetime field"""
--- uu/task/content.py
+++ uu/task/content.py
@@ -2,11 +2,11 @@ from plone.supermodel import model
 from zope import schema
 
 
-class IUUTask(model.Schema):
-    """Assignable Task With Due Date; Contains Dependent Tasks"""
+class UUTask(model.Schema):
+    """Assignable task with due date; may contain dependent tasks"""
     title = schema.TextLine(title=u"Name")
 
 
-class IUUTaskPlanner(model.Schema):
-    """Task Container; Shared Configuration For Contained Tasks"""
+class UUTaskPlanner(model.Schema):
+    """Task container; shared configuration for contained tasks"""
     title = schema.TextLine(title=u"Name")
