5e55794c8aa2e4ef269304af6ab305a16906f353
Alex Clark <aclark@aclark.net>
2015-06-20 09:51:22 -0400

    Add behavior
    Assigned Task has due date

--- uu/task/behaviors.py
+++ uu/task/behaviors.py
@@ -1,5 +1,13 @@
+from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
+from zope import schema
+from zope.interface import alsoProvides 
 
 
 class IUUAssignedTask(model.Schema):
-    """Adds due_on datetime field"""
+    """Adds due date field"""
+
+    due_on = schema.Datetime(title=u"Due Date")
+
+
+alsoProvides(IUUAssignedTask, IFormFieldProvider)
