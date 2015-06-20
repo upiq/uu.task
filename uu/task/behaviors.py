89a914915494bf18af639fce3339e6a5ae46d82e
Alex Clark <aclark@aclark.net>
2015-06-20 10:37:06 -0400

    Add behavior
    Computed Due Date Rules

--- uu/task/behaviors.py
+++ uu/task/behaviors.py
@@ -5,9 +5,25 @@ from zope.interface import alsoProvides
 
 
 class IUUAssignedTask(model.Schema):
-    """Adds due date field"""
+    """Due Date"""
 
     due_on = schema.Datetime(title=u"Due Date")
 
 
+class IUUTaskRules(model.Schema):
+    """Computed Due Date Rules"""
+
+    due_in = schema.Int()
+#    due_units = schema.Choice()
+#    due_rel = schema.Choice()
+#    source = schema.Choice()
+    use_dow = schema.Bool(title=u"Select relative to day of the week")
+    dow_n = schema.Int()
+#    dow = schema.Choice()
+    time_due = schema.Time(title=u"Time of day")
+#    notification_rules = schema.List()
+#    timezone = schema.Choice()
+
+
 alsoProvides(IUUAssignedTask, IFormFieldProvider)
+alsoProvides(IUUTaskRules, IFormFieldProvider)
