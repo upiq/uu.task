f78afa8de44a29f4edabd92c95b5d5f999f79e19
Alex Clark <aclark@aclark.net>
2015-06-20 19:03:58 -0400

    Add & Edit forms for Task & Task Planner
    Because we'll likely need them. Also add Task Planner perms and order ZCML to facilitate loading.

--- uu/task/content.py
+++ uu/task/content.py
@@ -1,16 +1,38 @@
 from .interfaces import ITask
 from .interfaces import ITaskPlanner
+from plone.dexterity.browser import add 
+from plone.dexterity.browser import edit 
 from plone.supermodel import model
 from zope.interface import implementer
 
 
+class TaskAddForm(add.DefaultAddForm):
+    """Custom add form for ``UU Task`` content type
+    """
+
+
+class TaskEditForm(edit.DefaultEditForm):
+    """Custom edit form for ``UU Task`` content type
+    """
+
+
+class TaskPlannerAddForm(add.DefaultAddForm):
+    """Custom add form for ``UU Task Planner`` content type
+    """
+
+
+class TaskPlannerEditForm(edit.DefaultEditForm):
+    """Custom edit form for ``UU Task Planner`` content type
+    """
+
+
 @implementer(ITask)
 class Task(model.Schema):
-    """Convenience subclass for ``UU Task`` portal type
+    """Convenience subclass for ``UU Task`` content type
     """
 
 
 @implementer(ITaskPlanner)
 class TaskPlanner(model.Schema):
-    """Convenience subclass for ``UU Task Planner`` portal type
+    """Convenience subclass for ``UU Task Planner`` content type
     """
