e0c6769488722748b77f45896bfc36ca11c98a40
Alex Clark <aclark@aclark.net>
2015-06-20 17:53:41 -0400

    Add IStartEnd
    Not quite sure how to use this yet

--- uu/task/interfaces.py
+++ uu/task/interfaces.py
@@ -22,6 +22,27 @@ class INotificationRule(Interface):
     source = schema.Choice([])
 
 
+class IStartEnd(Interface):
+    """
+    Task rule dependency on start, end date is a loose coupling
+    that is presumed duck-typed, as start and end may be either
+    Python date or datetime objects. This dependency is only
+    for calculation and not for storage of configuration
+    state.
+
+    Note: start/end for a task should have two supported
+    meanings:
+
+    (1) start, end bracker period for when task should be
+    permitted.
+
+    (2) start, end bracket some (often retrospective) period
+    of time related to the task to be performed.
+    """
+    start = schema.Datetime()
+    end = schema.Datetime()
+
+
 class ITask(Interface):
     """Explicit marker interface for UU Task
     """
