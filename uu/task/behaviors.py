217aecd70569308e6631e35e5e1b70a469f8e10e
Alex Clark <aclark@aclark.net>
2015-06-20 17:34:02 -0400

    ITaskRules is not a behavior

--- uu/task/behaviors.py
+++ uu/task/behaviors.py
@@ -10,20 +10,3 @@ class IAssignedTask(model.Schema):
     """
 
     due_on = schema.Datetime(title=u"Due Date")
-
-
-@provider(IFormFieldProvider)
-class ITaskRules(model.Schema):
-    """Computed due date
-    """
-
-    due_in = schema.Int()
-#    due_units = schema.Choice()
-#    due_rel = schema.Choice()
-#    source = schema.Choice()
-    use_dow = schema.Bool(title=u"Select relative to day of the week")
-    dow_n = schema.Int()
-#    dow = schema.Choice()
-    time_due = schema.Time(title=u"Time of day")
-#    notification_rules = schema.List()
-#    timezone = schema.Choice()
--- uu/task/interfaces.py
+++ uu/task/interfaces.py
@@ -1,3 +1,4 @@
+from zope import schema
 from zope.interface import Interface
 
 
@@ -9,3 +10,19 @@ class ITask(Interface):
 class ITaskPlanner(Interface):
     """Explicit marker interface for UU Task Planner
     """
+
+
+class ITaskRules(Interface):
+    """Computed due date
+    """
+
+    due_in = schema.Int()
+    due_units = schema.Choice([])
+    due_rel = schema.Choice([])
+    source = schema.Choice([])
+    use_dow = schema.Bool()
+    dow_n = schema.Int()
+    dow = schema.Choice([])
+    time_due = schema.Time()
+    notification_rules = schema.List()
+    timezone = schema.Choice([])
