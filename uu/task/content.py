664664b73392d5410c0b38c6f9fe18f50f7905b5
Alex Clark <aclark@aclark.net>
2015-06-21 16:46:47 -0400

    Fix edit forms

--- uu/task/content.py
+++ uu/task/content.py
@@ -1,5 +1,6 @@
 from .interfaces import ITask
 from .interfaces import ITaskPlanner
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.dexterity.browser import add
 from plone.dexterity.browser import edit
 from plone.supermodel import model
@@ -14,6 +15,7 @@ class TaskAddForm(add.DefaultAddForm):
 class TaskEditForm(edit.DefaultEditForm):
     """Custom edit form for Task
     """
+    template = ViewPageTemplateFile('templates/task_edit.pt')
 
 
 class TaskPlannerAddForm(add.DefaultAddForm):
@@ -24,6 +26,7 @@ class TaskPlannerAddForm(add.DefaultAddForm):
 class TaskPlannerEditForm(edit.DefaultEditForm):
     """Custom edit form for Task
     """
+    template = ViewPageTemplateFile('templates/taskplanner_edit.pt')
 
 
 @implementer(ITask)
