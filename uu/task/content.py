bdb80bbd2672364098ac9af5326229297e0c46b8
Alex Clark <aclark@aclark.net>
2015-06-21 07:53:55 -0400

    Wording

--- uu/task/content.py
+++ uu/task/content.py
@@ -7,32 +7,32 @@ from zope.interface import implementer
 
 
 class TaskAddForm(add.DefaultAddForm):
-    """Custom add form for ``UU Task`` content type
+    """Custom add form for Task
     """
 
 
 class TaskEditForm(edit.DefaultEditForm):
-    """Custom edit form for ``UU Task`` content type
+    """Custom edit form for Task
     """
 
 
 class TaskPlannerAddForm(add.DefaultAddForm):
-    """Custom add form for ``UU Task Planner`` content type
+    """Custom add form for Task Planner
     """
 
 
 class TaskPlannerEditForm(edit.DefaultEditForm):
-    """Custom edit form for ``UU Task Planner`` content type
+    """Custom edit form for Task
     """
 
 
 @implementer(ITask)
 class Task(model.Schema):
-    """Convenience subclass for ``UU Task`` content type
+    """Assignable task with due date
     """
 
 
 @implementer(ITaskPlanner)
 class TaskPlanner(model.Schema):
-    """Convenience subclass for ``UU Task Planner`` content type
+    """Series of tasks with shared settings
     """
