230328c898c1b8b0dbf34a39897ec2867d222978
Rok Garbas <rok@garbas.si>
2015-06-24 18:30:00 +0200

    added due_date, due_date_rule and notification_rules to IAssignedTask behavior

--- uu/task/behaviors.py
+++ uu/task/behaviors.py
@@ -1,12 +1,37 @@
+from plone.app.widgets.dx import DatetimeWidget
+from plone.autoform import directives as form
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
+from uu.task import _
 from zope import schema
 from zope.interface import provider
 
 
 @provider(IFormFieldProvider)
 class IAssignedTask(model.Schema):
-    """Due date
+    """adds Due date and notification_rules fields
     """
 
-    due_on = schema.Datetime(title=u"Due Date", required=False)
+    #model.fieldset(
+    #    'assigned',
+    #    label=_(u'Assigned'),
+    #    fields=('due_date', 'due_date_rule', 'notification_rules'),
+    #)
+
+    form.widget('due_date', DatetimeWidget)
+    due_date = schema.Datetime(
+        title=u"Due date",
+        required=False,
+    )
+
+    due_date_rule = schema.TextLine(
+        title=_(u"Computed due date"),
+        required=False,
+    )
+
+    notification_rules = schema.TextLine(
+        title=_(u"Notification rules"),
+        required=False,
+    )
+
+    # TODO: add invariant for due_date / due_date_rule
